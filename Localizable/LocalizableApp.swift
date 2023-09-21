d501990db25a6e1ed0a6ad9ac5ec1fc7bc4ff8a7
Akramjon <akramjon0175@gmail.com>
2023-09-21 10:16:44 -0700

    Initial Commit

---
+++ Localizable/LocalizableApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizableApp.swift
+//  Localizable
+//
+//  Created by Akramjon on 21/09/23.
+//
+
+import SwiftUI
+
+@main
+struct LocalizableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
